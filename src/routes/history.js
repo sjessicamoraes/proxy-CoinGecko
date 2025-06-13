6760bb4d47196fcdc05cae1c3f1e17c9ab7c94c0
Jessica Moraes <s.jessicamoraes@hotmail.com>
2025-06-13 12:55:15 -0300

    Add DELETE /history route to clear in-memory request log

--- src/routes/history.js
+++ src/routes/history.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
     res.json(history);
 });
 
+router.delete('/', (req, res) => {
+    history.length = 0;
+    res.status(200).json({ message: 'Histórico limpo com sucesso' });
+});
+
 export default router;
