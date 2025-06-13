b3f53d7e446253cc695566583097b818a1874bac
Jessica Moraes <s.jessicamoraes@hotmail.com>
2025-06-13 12:54:52 -0300

    correcting the function name

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import priceRoute from './src/routes/price.js';
-import statusRoute from './src/status.js';
-import historyRoute from './src/routes/history.js';
+import priceRouter from './src/routes/price.js';
+import statusRouter from './src/status.js';
+import historyRouter from './src/routes/history.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/price', priceRoute);
-app.use('/status', statusRoute);
-app.use('/history', historyRoute);
+app.use('/price', priceRouter);
+app.use('/status', statusRouter);
+app.use('/history', historyRouter);
 
 app.listen(PORT, () => {
   console.log(`API rodando em http://localhost:${PORT}`);
